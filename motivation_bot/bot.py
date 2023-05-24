c83b13db6184b5c3113cb069e99f0d843cfdf86f
Murcidum <73168797+Murcidum@users.noreply.github.com>
2023-05-24 19:57:28 +0500

    Update bot.py
    commit 1 #первая версия бота, список команд и ответ на них

--- motivation_bot/bot.py
+++ motivation_bot/bot.py
@@ -1,1 +1,19 @@
 # В этом файле находится основная логика телеграмм бота
+import telebot
+
+bot = telebot.TeleBot('5996242526:AAGRh-F7JJzW4sCOQzdwvVeHnmAmDn-J4KU')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, f'Hi, {message.from_user.first_name} !')
+
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, 'Some instructions!')
+
+@bot.message_handler()
+def error(message):
+    bot.send_message(message.chat.id, 'Error, I don\'t have such a command!')
+
+bot.polling(none_stop=True)
